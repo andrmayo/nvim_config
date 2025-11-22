9489ef1758c9155f736677db7d919723b52319ed
andrmayo <acmayo399@gmail.com>
2025-11-22 01:50:06 -0500

    reconfigured Telescope key mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "master",  -- Use latest version instead of old tag
+  branch = "master", -- Use latest version instead of old tag
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -9,16 +9,17 @@ return {
     require("telescope").setup({
       defaults = {
         preview = {
-          treesitter = false,  -- Disable treesitter in previews to avoid ft_to_lang error
+          treesitter = false, -- Disable treesitter in previews to avoid ft_to_lang error
         },
       },
     })
 
-    vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+    vim.keymap.set("n", "<space>/", require("telescope.builtin").live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files, { desc = "Telescope find in dir" })
     vim.keymap.set("n", "<space>en", function()
       require("telescope.builtin").find_files({
         cwd = vim.fn.stdpath("config"),
       })
-    end)
+    end, { desc = "Telescope edit nvim" })
   end,
 }
