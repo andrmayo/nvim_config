fa6d98ae0e403189d4b3362e2b068fc4d3f52a55
Andrew Mayo <acmayo399@gmail.com>
2026-02-02 15:58:28 -0500

    added <C-b> key mapping for Telescope pickers to open file in new buffer without closing Telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,8 +6,34 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
+    local action_state = require("telescope.actions.state")
+    local from_entry = require("telescope.from_entry")
+
+    local function open_in_background(_)
+      local entry = action_state.get_selected_entry()
+      if not entry then
+        return
+      end
+      local filepath = from_entry.path(entry, false, false)
+      if filepath then
+        local bufnr = vim.fn.bufadd(filepath)
+        vim.fn.bufload(bufnr)
+        vim.bo[bufnr].buflisted = true
+      end
+    end
+
     require("telescope").setup({
       defaults = {
+        mappings = {
+          i = {
+            ["<C-h>"] = "which_key",
+            ["<C-b>"] = open_in_background,
+          },
+          n = {
+            ["<C-h>"] = "which_key",
+            ["<C-b>"] = open_in_background,
+          },
+        },
         preview = {
           treesitter = false, -- Disable treesitter in previews to avoid ft_to_lang error
         },
